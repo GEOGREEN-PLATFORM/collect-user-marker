1e856e1ac7128551c73a7458842e243061c20ab1
Egor <dementev.eg@yandex.ru>
2024-12-18 01:28:21 +0300

    Create project

---
+++ src/main/java/com/example/collect_user_data/CollectUserDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.collect_user_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectUserDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectUserDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/collect_user_data/CollectUserDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collect_user_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectUserDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
