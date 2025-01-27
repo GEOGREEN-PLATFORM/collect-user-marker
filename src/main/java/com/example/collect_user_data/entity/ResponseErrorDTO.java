67bd81ea98126e28bbab3e50bd8b9a6646d89ace
Egor <dementev.eg@yandex.ru>
2025-01-27 23:42:13 +0300

    renamed and remove validation

--- src/main/java/com/example/collect_user_data/entity/ResponseErrorDTO.java
+++ src/main/java/com/example/collect_user_data/entity/ResponseErrorDTO.java
@@ -1,7 +1,5 @@
 package com.example.collect_user_data.entity;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +8,9 @@ import org.springframework.http.HttpStatus;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ResponseErrorEntity {
+public class ResponseErrorDTO {
 
-    @NotNull
     private HttpStatus status;
 
-    @NotBlank
     private String message;
 }
